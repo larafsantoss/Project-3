852f21471641116422e1aff935efc84ca354118c
larafsantoss@gmail.com <larafsantoss@gmail.com>
2022-09-10 00:59:19 +1000

    simple contact page

--- client/js/components/header.js
+++ client/js/components/header.js
@@ -1,12 +1,14 @@
 import { renderMenuList } from "./menu.js";
 import { renderOurStory } from "./our-story.js";
+import { renderHome } from "./home.js";
+import { renderContact } from "./contact.js";
 
 export const renderHeader = () => {
   const header = document.querySelector("#header-nav");
   header.innerHTML = `
     <h1>Cake Shop</h1>
     <ul id="navlist">
-      <li>Home</li>
+      <li id="home">Home</li>
       <li id="menu">Menu</li>
       <li id="story">Our Story</li>
       <li id="contact">Contact</li>
@@ -18,4 +20,10 @@ export const renderHeader = () => {
 
   const story = document.getElementById("story");
   story.addEventListener("click", renderOurStory);
+
+  const home = document.getElementById("home");
+  home.addEventListener("click", renderHome);
+
+  const contact = document.getElementById("contact");
+  contact.addEventListener("click", renderContact);
 };
---
+++ client/js/components/home.js
@@ -0,0 +1,5 @@
+export const renderHome = () => {
+    const home = document.createElement("p");
+    home.textContent = '' 
+    page.replaceChildren(home);
+  };
