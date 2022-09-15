4c7cb1477ed266edab6b85f19b6bc1808b05745b
Julie Nguyen <julie.hyen@gmail.com>
2022-09-15 14:52:35 +1000

    Update order details

--- client/js/index.js
+++ client/js/index.js
@@ -1,5 +1,5 @@
 import { renderHeader } from "./components/header.js";
-localStorage.clear(); // TO DELETE
+// localStorage.clear(); // TO DELETE
 
 const cartItems = JSON.parse(localStorage.getItem("cartItems"));
 if (!cartItems) {
--- controllers/orders.js
+++ controllers/orders.js
@@ -11,10 +11,16 @@ router.get("/api/orders", (req, res) => {
   });
 });
 
-router.post("/api/orders", (req, res) => {
-  const { customerName, customerAddress, totalAmount } = req.body;
+router.get("/api/items/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = "SELECT * FROM orders WHERE id=$1";
+  db.query(sql, [id]).then((dbRes) => {
+    res.json(dbRes.rows.length === 0 ? {} : dbRes.rows[0]);
+  });
+});
 
-  // console.log(req.body);
+router.post("/api/orders", (req, res) => {
+  const { customerName, customerAddress, totalAmount, orderDetails } = req.body;
 
   if (customerName === "" || customerAddress === "") {
     return res.status(400).join({ message: "Please fill in the entire form" });
@@ -24,7 +30,17 @@ router.post("/api/orders", (req, res) => {
     "INSERT INTO orders(customer_name, customer_address, total_amount) VALUES($1, $2, $3) RETURNING id";
   db.query(sql, [customerName, customerAddress, totalAmount])
     .then((dbRes) => {
-      res.json({ orderId: dbRes.rows[0].id });
+      const orderId = dbRes.rows[0].id;
+      const sql =
+        "INSERT INTO order_details(order_id, product_id, quantity, unit_price_in_cents) VALUES($1, $2, $3)";
+      db.query(sql, [
+        orderId,
+        orderDetails.itemId,
+        orderDetails.quantity,
+        orderDetails.unitPriceInCents,
+      ]).then((dbRes) => {
+        res.json({ orderId });
+      });
     })
     .catch((err) => {
       res.status(500).json({});
