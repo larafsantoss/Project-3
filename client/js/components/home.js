b05a5b93b9065538f7a95787b25d02b36c596e73
larafsantoss@gmail.com <larafsantoss@gmail.com>
2022-09-17 08:40:49 +1000

    readme file

--- client/js/components/home.js
+++ client/js/components/home.js
@@ -71,7 +71,7 @@ export const renderHome = () => {
             following for its Debugging cupcakes and cookies. Our team is composed of
             16 bakers that were trying to become software developers, but something
             went wrong in "a 3rd Project" so we gave up and decide to cook delicious
-            cakes using ingredients from Lucys farm supplier. Not every day is
+            cakes using ingredients from Lucy&Sam's farm supplier. Not every day is
             perfect, some days Sid is not there to help us out, some days Ken and Ge
             (our regular customers) order more than we can cook. It is the good days
             we live for though. Sometimes we do not even remember that we had to use
